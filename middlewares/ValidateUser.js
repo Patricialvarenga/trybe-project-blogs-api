3317d9d3ad099690865141cc67158f57a5bd94b0
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2022-01-07 13:30:55 -0300

    altera req 1

--- middlewares/ValidateUser.js
+++ middlewares/ValidateUser.js
@@ -7,11 +7,11 @@ const name = (req, res, next) => {
     next();
 };
 
+// Source: https://forum.blip.ai/t/resolvido-regex-para-validacao-de-email/1635
 const email = (req, res, next) => {
     const { email: valueEmail } = req.body;
-    const errorMessage = '"email" is required';
-    if (!valueEmail) return res.status(400).json({ message: errorMessage });
-    // Source: https://forum.blip.ai/t/resolvido-regex-para-validacao-de-email/1635
+    const requiredMessage = '"email" is required';
+    if (!valueEmail) return res.status(400).json({ message: requiredMessage });
     const regex = /^([\w.-]+)@([\w-]+)((\.(\w){2,3})+)$/;
     const validMessage = '"email" must be a valid email';
     if (!regex.test(valueEmail)) return res.status(400).json({ message: validMessage });
@@ -21,15 +21,15 @@ const email = (req, res, next) => {
 const registeredEmail = async (req, res, next) => {
     const { email: valueEmail } = req.body;
     const users = await User.findAll({ where: { email: valueEmail } });
-    const errorMessage = 'User already registered';
-    if (users.length > 0) return res.status(409).json({ errorMessage });
+    const message = 'User already registered';
+    if (users.length > 0) return res.status(409).json({ message });
     next(); 
 };
 
 const password = (req, res, next) => {
     const { password: valuePassword } = req.body;
-    const errorMessage = '"password" is required';
-    if (!valuePassword) return res.status(400).json({ message: errorMessage });
+    const requiredMessage = '"password" is required';
+    if (!valuePassword) return res.status(400).json({ message: requiredMessage });
     const validMessage = '"password" length must be 6 characters long';
     if (valuePassword.length !== 6) return res.status(400).json({ message: validMessage });
     next();
